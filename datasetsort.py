4b0f7369d1fbb49b1e481f00b3e60ac3c6eb3c01
samanthalrobbins <54602849+samanthalrobbins@users.noreply.github.com>
2020-04-22 00:45:47 -0500

    Update datasetsort.py

--- datasetsort.py
+++ datasetsort.py
@@ -15,21 +15,25 @@ def constant_df(df):
     df = df[~df["text"].str.contains("|".join(app_key))]
     return df
     
-def comm_df(df):
-    app_key = ["commerical", "commercials", " ad ", "advertisement"]
-    app_keymobile = [" app ","application"," mobile ", "drive safe and save", "#drivesafeandsave", "drive safe", "smartphone", "iphone"]
-    df = df[~df["text"].str.contains("|".join(app_key))]
-    df = df[~df["text"].str.contains("|".join(app_keymobile))]
-    return df            
+def nd(df):
+    tropicalstorm= ["landslide", "imelda","tropical storm","storm michael", "severe storms", "severe storm", "flooding", ]    
+    hurricane = ["hurricane", "dorian","barry" ]
+    earthquake =["earthquake" ]
+    tornado = ["tornado"]
+    df = df[df["text"].str.contains("|".join(tropicalstorm))]
+    df = df[df["text"].str.contains("|".join(hurricane))]
+    df = df[df["text"].str.contains("|".join(earthquake))]
+    df = df[df["text"].str.contains("|".join(tornado))]
+    return df           
    
 def main():
     df = read_file('removal.csv')
     mobiledf = mobile_df(df)
     constantdf = constant_df(df)
-    commdf = comm_df(df)
+    nddf = nd(df)
     mobiledf.to_csv(r'mobile.csv', index = False)
     constantdf.to_csv(r'base.csv', index = False)
-    commdf.to_csv(r'no_commerical_base.csv', index = False)
+    nddf.to_csv(r'naturaldisasters.csv', index = False)
     
     #print(df)
 
