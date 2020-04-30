60f5799a800319b24b0f0c92ad12a5641b0549f2
Sami <samanthalrobbins@email.arizona.edu>
2020-04-29 19:17:49 -0500

    add three datasets

--- datasetsort.py
+++ datasetsort.py
@@ -1,41 +1,58 @@
 import pandas as pd
 
 def read_file(file):
-    df = pd.read_csv(file)
-
+    df = pd.read_csv(file, encoding='utf-8-sig')
     return df
 
+'''
+finds all the mobile application tweets
+'''
 def mobile_df(df):
     app_key = [" app ","application"," mobile ", "drive safe and save", "#drivesafeandsave", "drive safe", "smartphone", "iphone"]
-    df = df[df["text"].str.contains("|".join(app_key))]
+    df = df[df.text.str.contains("|".join(app_key))]
     return df
 
-def constant_df(df):
-    app_key = [" app ","application"," mobile ", "drive safe and save", "#drivesafeandsave", "drive safe", "smartphone", "iphone"]
-    df = df[~df["text"].str.contains("|".join(app_key))]
-    return df
+'''
+finds all the natural disaster tweets
+'''
+def nd_df(df):
+    nd_key = ["landslide", "imelda","tropical storm","storm michael", "severe storms", "severe storm", "flooding", "flood","hurricane", "dorian","barry","fernand","earthquake","tornado","fire","damage", "disaster"]
     
-def nd(df):
-    tropicalstorm= ["landslide", "imelda","tropical storm","storm michael", "severe storms", "severe storm", "flooding", ]    
-    hurricane = ["hurricane", "dorian","barry" ]
-    earthquake =["earthquake" ]
+    '''
+    tropicalstorm= ["landslide", "imelda","tropical storm","storm michael", "severe storms", "severe storm", "flooding", "flood"]    
+    hurricane = ["hurricane", "dorian","barry","fernand" ]
+    earthquake =["earthquake"]
     tornado = ["tornado"]
-    df = df[df["text"].str.contains("|".join(tropicalstorm))]
-    df = df[df["text"].str.contains("|".join(hurricane))]
-    df = df[df["text"].str.contains("|".join(earthquake))]
-    df = df[df["text"].str.contains("|".join(tornado))]
+    fire = ["fire"]
+    general = ["damage", "disaster"]
+    '''
+    df = df[df.text.str.contains('|'.join(nd_key))]
     return df           
-   
+  
+'''
+State Farm as a whole, exclusive of the other two categories
+''' 
+def sf_df(df):
+    app_key = [" app ","application"," mobile ", "drive safe and save", "#drivesafeandsave", "drive safe", "smartphone", "iphone"]
+    nd_key = ["landslide", "imelda","tropical storm","storm michael", "severe storms", "severe storm", "flooding", "flood","hurricane", "dorian","barry","fernand","earthquake","tornado","fire","damage", "disaster"]
+    df = df[~df.text.str.contains("|".join(app_key))]
+    df = df[~df.text.str.contains("|".join(nd_key))]
+    return df
+        
+
 def main():
-    df = read_file('removal.csv')
-    mobiledf = mobile_df(df)
-    constantdf = constant_df(df)
-    nddf = nd(df)
-    mobiledf.to_csv(r'mobile.csv', index = False)
-    constantdf.to_csv(r'base.csv', index = False)
-    nddf.to_csv(r'naturaldisasters.csv', index = False)
+    df = read_file('cleaned_data.csv')
+    mobile = mobile_df(df)
+    nd = nd_df(df)
+    sf = sf_df(df)
+    
+    mobile.to_csv(r'mobile.csv', index = False)
+    nd.to_csv(r'naturaldisasters.csv', index = False)
+    sf.to_csv(r'statefarm.csv', index = False)
+    
     
     #print(df)
 
 if __name__ == '__main__':
     main()
+
