562d6b2356840b3203dc7d2430e7f923ba2bdff7
evenegas3 <evenegas3@users.noreply.github.com>
2020-05-04 06:25:09 -0700

    2019data

--- final_fetch.py
+++ final_fetch.py
@@ -98,8 +98,121 @@ def real_main(state):
     final_df.to_csv('24km_{}.csv'.format(state), sep='\t', encoding='utf-8')
     print('$$$$$')
 
-s = 'Illinois'
-delete_empty(s)
-real_main(s)
-rename_cols(s)
+# s = 'Illinois'
+# delete_empty(s)
+# real_main(s)
+# rename_cols(s)
 
+
+def all_og_df():
+    df = pd.read_csv('all_geo_2019.csv')
+
+    df['merged'] = df['date'].fillna(df['timestamp'])
+    df['likefav'] = df['likes'].fillna(df['favorites'])
+    # df['mreply'] = df['replay_to_users'].fillna(df['reply_to_users'])
+
+    df = df.drop(columns=['has_media', 'img_urls', 'video_url', 'timestamp_epochs', 'links', 'likes', 'favorites', 'parent_tweet_id', 'timestamp', 'date', 'username', 'is_replied', 'is_reply_to', 'text_html', 'permalink', 'city_id', 'city'])
+    df = df.rename(columns={'merged': 'date', 'likefav':'favorites'})
+
+
+    # print(df)
+    # for i, row in df.iterrows():
+    #     print(row)
+    #     input()
+    
+    # df = df[df.columns[::-1]]
+    df.to_csv('all_2019.csv', index=False)
+
+all_og_df()
+# def update2():
+#     df = pd.read_csv('all_geo_2019.csv')
+    # df['likefav'] = df['likes'].fillna(df['favorites'])
+#     # df['merge_reply'] = df['reply_to_users'].fillna(df['to'])
+
+
+#     df = df.drop(columns=['timestamp', 'date', 'likes', 'favorites', 'username'])
+#     df = df.rename(columns={'merged': 'date', 'likefav':'favorites'})
+
+
+# update2()
+# all_og_df()
+
+# def reformat():
+#     count = 0
+
+    # df.to_csv('update1.csv', index=False)
+
+
+
+    # l = [x for x in os.listdir('errors/')]
+    # print(len(l))
+
+    # combine = []
+    # for file in os.listdir('unordered_data/'):
+    #     # print(file)
+        # df = pd.read_csv('unordered_data/{}'.format(file))
+    #     # all_cols = list(df)
+    #     combine.append(df)
+    
+    # frame = pd.concat(combine, axis=0, ignore_index=True)
+    # # frame = df[df.columns[::-1]]
+    # print(frame)
+    # frame.to_csv('all_29', index=False)
+
+
+
+
+
+        # print(df)
+        # print(df)
+        # df.to_csv('{}'.format(file), index=False)
+
+        # if file == 'final_Idaho2019.csv':
+        #     df = pd.read_csv('errors/{}'.format(file), sep='\t')
+        #     # print(df)
+            # all_cols = list(df)
+            # first = df.columns[0]
+
+            # for name in all_cols:
+            #     if 'Unnamed' in name:
+            #         df = df.drop(columns=[name])
+
+            # print(df)
+            # df.to_csv('{}'.format(file), index=False)
+
+        # bad = ['final_NorthDakota2019.csv']# 'final_Idaho2019.csv']
+
+        # if file in bad:
+        #     continue
+        # print(file)
+        # if file == 'final_NorthDakota2019.csv':
+        #     df = pd.read_csv('errors/{}'.format(file), sep='\t')
+
+            # all_cols = list(df)
+            # first = df.columns[0]
+
+            # for name in all_cols:
+            #     if 'Unnamed' in name:
+            #         df = df.drop(columns=[name])
+
+            # print(df)
+            # df.to_csv('24km_{}.csv'.format(file), index=False)
+
+
+        # print(file)
+        # df = pd.read_csv('errors/{}'.format(file))
+        # all_cols = list(df)
+        # first = df.columns[0]
+
+        # for name in all_cols:
+        #     if 'Unnamed' in name:
+        #         df = df.drop(columns=[name])
+
+        # if first == 'city':
+        #     df = df[df.columns[::-1]]
+            
+        #     # , sep='delimiter'
+        # print(df)
+
+
+# reformat()
